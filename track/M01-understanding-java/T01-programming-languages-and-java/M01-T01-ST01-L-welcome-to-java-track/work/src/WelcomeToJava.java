c731d447ec66529225cfd51d97a717649a7845d7
Deepa <routaggnideepa@gmail.com>
2026-08-04 23:44:23 +0530

    first

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -2,8 +2,24 @@
 public class WelcomeToJava {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to java track");
-        System.out.println("I already know to build programming language");
-        System.out.println("I am a Software Developer");
+        //System.out.println("Monday");
+        //System.out.print("Tuesday");
+        //System.out.println("Wednesday");
+        //System.out.println("Thursday");
+        //System.out.print("Friday");
+        //System.out.println("Saturday");
+        //System.out.print("Sunday"); 
+
+        // conditional statements
+        int num = 25;
+        if(num > 0){
+            System.out.println("positive");
+        }
+        else if(num < 0){
+            System.out.println("negative");
+        }
+        else{
+            System.out.println("zero");
+        }
     }
-}
+} 
